aae42394ff0c5b9197aedf5bf9703d80f811551e
Лобченко Олександр <sashapro712@gmail.com>
2024-02-04 19:41:02 +0100

    Adding ShowEmployeeInfoCommand

---
+++ Commands/ShowEmployeeInfoCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szpital.Commands
+{
+    public class ShowEmployeeInfoCommand : CommandBase
+    {
+        public ShowEmployeeInfoCommand()
+        {
+
+        }
+
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
