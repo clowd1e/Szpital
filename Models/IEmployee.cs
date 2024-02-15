5bd0f40c35f73950aa91984c890a2b72b094a1b1
Лобченко Олександр <61800741+clowd1e@users.noreply.github.com>
2024-02-15 19:27:02 +0100

    Update IEmployee.cs

--- Models/IEmployee.cs
+++ Models/IEmployee.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,14 +12,7 @@ namespace Szpital.Models
         public int MedicineDepartment { get; set; }
         public string Position { get; set; }
         public string? Specialty { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Pesel { get; set; }
-        public string PhoneNumber { get; set; }
         public string Email { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
         public DateTime EmploymentDate { get; set; }
         public decimal Salary { get; set; }
         public bool IsDeleted { get; set; }
