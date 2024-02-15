5ece0dd3c5a7dd063880973a8d1cd741b57abd8b
Лобченко Олександр <61800741+clowd1e@users.noreply.github.com>
2024-02-15 19:29:38 +0100

    Update DoctorVisitsViewModel.cs

--- ViewModels/Doctor/DoctorVisitsViewModel.cs
+++ ViewModels/Doctor/DoctorVisitsViewModel.cs
@@ -1,13 +1,28 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Szpital.DbContexts;
+using Szpital.Models;
+using Szpital.Stores;
 
 namespace Szpital.ViewModels.Doctor
 {
     public class DoctorVisitsViewModel : ViewModelBase
     {
-        public DoctorVisitsViewModel() { }
+        private readonly NavigationStore navigationStore;
+        private readonly MainViewModel mainViewModel;
+        private readonly Employee employee;
+        private readonly Account account;
+        public ObservableCollection<VisitViewModel> Visits => DbContext.GetVisits(employee.EmployeeId);
+        public DoctorVisitsViewModel(NavigationStore navigationStore, MainViewModel mainViewModel, Employee employee, Account account)
+        {
+            this.navigationStore = navigationStore;
+            this.mainViewModel = mainViewModel;
+            this.employee = employee;
+            this.account = account;
+        }
     }
 }
